3e306efdce7b6316a1d73961e631b5cdc8e20320
ZengN <pp654304469@163.com>
2017-04-03 13:50:00 +0800

    index.html
    index 更新

--- qqdancer/js/index.js
+++ qqdancer/js/index.js
@@ -1,3 +1,22 @@
 $(function(){
+	$('#box-head-right').click(function() {
+		$(this).addClass("btn-change").siblings().removeClass('btn-change');
+		$('#main2').show();
+		$('#main1').hide();
+	});
 	
+	$('#box-head-left').click(function() {
+		$(this).addClass("btn-change").siblings().removeClass('btn-change');
+		$('#main1').show();
+		$('#main2').hide();
+	});
+	
+	$('#btn1').click(function() {
+		$('#popup-box').show();
+		$('#popup-color').show();
+	});
+	
+	$('#close').click(function() {
+		$('#popup-box').hide();
+	});
 });
