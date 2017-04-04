7349ca335062a1c124b11c785dcb21ac685aa293
ZengN <pp654304469@163.com>
2017-04-04 22:09:15 +0800

    index.html
    QQ炫舞人缘大比拼页面重构

--- qqdancer/js/index.js
+++ qqdancer/js/index.js
@@ -1,12 +1,12 @@
 $(function(){
 	$('#box-head-right').click(function() {
-		$(this).addClass("btn-change").siblings().removeClass('btn-change');
+		$(this).addClass("btn-change2").siblings().removeClass('btn-change1');
 		$('#main2').show();
 		$('#main1').hide();
 	});
 	
 	$('#box-head-left').click(function() {
-		$(this).addClass("btn-change").siblings().removeClass('btn-change');
+		$(this).addClass("btn-change1").siblings().removeClass('btn-change2');
 		$('#main1').show();
 		$('#main2').hide();
 	});
@@ -16,7 +16,8 @@ $(function(){
 		$('#popup-color').show();
 	});
 	
-	$('#close').click(function() {
+	$('#closebtn').click(function() {
 		$('#popup-box').hide();
+		$('#popup-color').hide();
 	});
 });
